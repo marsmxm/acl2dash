ca56ea9ca700bd01fa3c45c6eb98a74ff202883d
Mu Xian Ming <marsmxm@gmail.com>
2024-04-05 21:15:38 +0800

    v8.5

--- query.py
+++ query.py
@@ -3,6 +3,11 @@ import sqlite3
 connection = sqlite3.connect("ACL2.docset/Contents/Resources/docSet.dsidx")
 
 cursor = connection.cursor()
+
+rows = cursor.execute(
+    "SELECT count(*) FROM searchIndex ").fetchall()
+print(rows)
+
 rows = cursor.execute(
     "SELECT * FROM searchIndex WHERE name='Defun' LIMIT 10").fetchall()
 print(rows)
